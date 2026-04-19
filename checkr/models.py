3077962ee822bfaa61ec1de040fd3fa352a1b73e
mammadu <mammadudiallo@gmail.com>
2026-04-19 03:36:18 +0000

    add csv, create model based on csv, add tests to make sure csv and model match

--- checkr/models.py
+++ checkr/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Customer(models.Model):
+	index = models.IntegerField(primary_key=True)
+	customer_id = models.CharField(max_length=15)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	company = models.CharField(max_length=100)
+	city = models.CharField(max_length=200)
+	country = models.CharField(max_length=100)
+	phone_1 = models.CharField(max_length=50)
+	phone_2 = models.CharField(max_length=50)
+	email = models.EmailField()
+	subscription_date = models.DateField()
+	website = models.URLField()
--- checkr/tests.py
+++ checkr/tests.py
@@ -1,3 +1,29 @@
+import csv
+from pathlib import Path
 from django.test import TestCase
-from .models import User
+from .models import Customer
 
+
+CURRENT_DIR = Path(__file__).resolve().parent
+
+class CSVImporterTests(TestCase):
+    """
+    for testing functions realted to CSV
+    """
+    def test_csv_and_model_object_have_same_headers(self, csv_path: str = CURRENT_DIR / "customers-100.csv"):
+        """
+        make sure the headers for the db and the csv match"
+        """
+        customer_fields = [field.name for field in Customer._meta.fields]
+
+        with open(csv_path, "r") as file:
+            reader = csv.reader(file)
+            headers = next(reader)
+
+        # convert header to lowercase and change spaces to underscores
+        for index, header in enumerate(headers):
+            header = header.lower()
+            header = "_".join(header.split())
+            headers[index] = header
+
+        self.assertEqual(customer_fields, headers)
